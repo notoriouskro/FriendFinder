b25feebe4f242b0dc7f78d4d4dda0257a4fd0b6e
Kyle Robert <notoriouskro@gmail.com>
2019-03-29 22:29:29 -0700

    comparison

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var friendsData = [
+    {
+    name: 'Kyle Robert',
+    photo:
+        'https://images.fineartamerica.com/images-medium-5/will-ferrell-old-school-tony-rubino.jpg',
+    scores: ['5', '4', '1', '1', '3', '5', '3', '5', '5', '5']
+},
+{
+    name: 'Matt Sainson',
+    photo:
+        'https://images.fineartamerica.com/images-medium-5/will-ferrell-old-school-tony-rubino.jpg',
+    scores: ['5', '2', '1', '1', '3', '5', '2', '5', '4', '1']
+},
+{
+    name: 'Marvin Tryon',
+    photo:
+        'https://images.fineartamerica.com/images-medium-5/will-ferrell-old-school-tony-rubino.jpg',
+    scores: ['1', '4', '1', '5', '3', '5', '3', '1', '5', '3']
+}
+
+];
+
+module.exports = friendsData
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,29 +4,63 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friendsData = require("../data/friends");
 
-
+var userData = require("../data/friends");
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
+  
+
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
+  app.get("/api/friends", function (req, res) {
+
+    res.json(userData);
   });
 
 
 
-  app.post("/api/friends", function(req, res) {
-    res.send("Something");
+  app.post("/api/friends", function (req, res) {
+    var newUser = req.body;
+    console.log(newUser);
+
+    //Lowest score wins
+    //Set newUser.id to userData.length
+    //each element in userData
+    //each element in userData.scores
+    // Compare each answer to the users currently in the array
+    //clear totalScore = 0
+    //ABS of userData subtract newUser 
+    //Add each value to total score
+
+    var lowestScore = 1000;
+    var totalScore = 0;
+    userData.forEach(function(user){
+      totalScore = 0;
+      user.scores.forEach(function(score){
+        totalScore = Math.abs(score - newUser.scores);
+        console.log("New user score", newUser.scores);
+      });
+      console.log("Total Score", totalScore);
+      if (totalScore < lowestScore) {
+        lowestScore = totalScore;
+        lowestIdx = i;
+      }
+    });
+
+
+    // Now push newUser into the friends array
+    userData.push(newUser);
+
+    var data = userData[lowestIdx];
+    res.send(data);
     res.end();
   });
 
